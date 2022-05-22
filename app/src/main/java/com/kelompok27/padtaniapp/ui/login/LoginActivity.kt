528babb8c427b5274c641990b6af91e1d74cba8a
dickyarya <dickyarya170101@gmail.com>
2022-05-23 02:48:33 +0700

    center title toolbar

--- app/src/main/java/com/kelompok27/padtaniapp/ui/login/LoginActivity.kt
+++ app/src/main/java/com/kelompok27/padtaniapp/ui/login/LoginActivity.kt
@@ -3,15 +3,27 @@ package com.kelompok27.padtaniapp.ui.login
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
+import androidx.appcompat.widget.Toolbar
+import com.kelompok27.padtaniapp.R
 import com.kelompok27.padtaniapp.databinding.ActivityLoginBinding
 import com.kelompok27.padtaniapp.ui.main.MainActivity
 
 class LoginActivity : AppCompatActivity() {
     private lateinit var binding: ActivityLoginBinding
+    private lateinit var customToolbar: Toolbar
+    private lateinit var textToolbar: TextView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityLoginBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        customToolbar= findViewById(R.id.toolbar)
+        textToolbar = findViewById(R.id.toolbarTitle)
+
+        setSupportActionBar(customToolbar)
+        textToolbar.text = "Sign in"
 
         binding.btnLogin.setOnClickListener {
             intentToMain()
@@ -23,4 +35,5 @@ class LoginActivity : AppCompatActivity() {
             startActivity(it)
         }
     }
+
 }
