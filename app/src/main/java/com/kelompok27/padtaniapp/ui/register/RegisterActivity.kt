ca3d82f9594b9f985104a090d979d9da613c6027
4nwar <sadad279@gmail.com>
2022-05-23 00:56:56 +0700

    registerActivity

---
+++ app/src/main/java/com/kelompok27/padtaniapp/ui/register/RegisterActivity.kt
@@ -0,0 +1,14 @@
+package com.kelompok27.padtaniapp.ui.register
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.kelompok27.padtaniapp.databinding.ActivityRegisterBinding
+
+class RegisterActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityRegisterBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityRegisterBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+    }
+}
