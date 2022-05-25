4a185b7fcfbb0878a1498e47a172f250abdfca97
dickyarya <dickyarya170101@gmail.com>
2022-05-25 10:44:17 +0700

    add custom tablayout

--- app/src/main/java/com/kelompok27/padtaniapp/ui/main/ui/home/HomeFragment.kt
+++ app/src/main/java/com/kelompok27/padtaniapp/ui/main/ui/home/HomeFragment.kt
@@ -5,27 +5,12 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.annotation.StringRes
-import androidx.viewpager2.widget.ViewPager2
-import com.google.android.material.tabs.TabLayout
-import com.google.android.material.tabs.TabLayoutMediator
 import com.kelompok27.padtaniapp.R
 import com.kelompok27.padtaniapp.databinding.FragmentHomeBinding
-import com.kelompok27.padtaniapp.ui.helper.SectionsPagerAdapter
 
 class HomeFragment : Fragment() {
 
     private lateinit var binding: FragmentHomeBinding
-
-    companion object {
-        @StringRes
-        private val TAB_TITLES = intArrayOf(
-            R.string.inpari,
-            R.string.inpago,
-            R.string.Hipa,
-            R.string.Inpara
-        )
-    }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
