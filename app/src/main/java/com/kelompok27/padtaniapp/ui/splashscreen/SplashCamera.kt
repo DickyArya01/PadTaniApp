d4876d4a3b058222d8a7db0e33b3dea6b00709b2
4nwar <sadad279@gmail.com>
2022-06-11 17:16:32 +0700

    add raw lottiefiles

---
+++ app/src/main/java/com/kelompok27/padtaniapp/ui/splashscreen/SplashCamera.kt
@@ -0,0 +1,12 @@
+package com.kelompok27.padtaniapp.ui.splashscreen
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.kelompok27.padtaniapp.R
+
+class SplashCamera : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash_camera)
+    }
+}
